ec546dbf96c266a242f7f1ad74e693068417aab3
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-29 15:49:37 +0000

    chore(deps): Updated

--- src/warehouse/index.ts
+++ src/warehouse/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.67.0/docs/resources/warehouse
 // generated from terraform resource schema
 
