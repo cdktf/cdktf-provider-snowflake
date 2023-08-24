43ea7949cfcaa0f727c2ddacbe83e72b2bc84e27
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-08-24 16:00:03 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-snowflake-sequences/index.ts
+++ src/data-snowflake-sequences/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.70.0/docs/data-sources/sequences
 // generated from terraform resource schema
 
