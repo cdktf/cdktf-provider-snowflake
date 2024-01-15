ef5c52a0186085339c4261e5cc51232e55594478
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-15 11:49:19 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-snowflake-external-functions/index.ts
+++ src/data-snowflake-external-functions/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.83.1/docs/data-sources/external_functions
 // generated from terraform resource schema
 
