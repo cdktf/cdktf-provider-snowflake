d44be0407760de998c8f985e87d5f93b1d5f10d7
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-02-19 12:40:17 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@9dd7bfd

--- src/table-grant/index.ts
+++ src/table-grant/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.86.0/docs/resources/table_grant
 // generated from terraform resource schema
 
