c912374d73c727c775d73099318a118cbf25b7ad
Team Terraform CDK <github-team-tf-cdk@hashicorp.com>
2024-03-23 03:02:58 +0000

    fix: upgrade provider from `0.87.2` to version `0.87.2`
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-snowflake-current-account/index.ts
+++ src/data-snowflake-current-account/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.87.2/docs/data-sources/current_account
 // generated from terraform resource schema
 
