08a28eb36276187594aef359ec502b347dc07537
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-29 08:12:00 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/task-grant/index.ts
+++ src/task-grant/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.89.0/docs/resources/task_grant
 // generated from terraform resource schema
 
