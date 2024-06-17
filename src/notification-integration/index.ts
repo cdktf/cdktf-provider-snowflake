be2e9b1892ba11e63839c03f6ec1dd2af83a4b03
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-17 14:13:15 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/notification-integration/index.ts
+++ src/notification-integration/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.92.0/docs/resources/notification_integration
 // generated from terraform resource schema
 
