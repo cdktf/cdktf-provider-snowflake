1fdf79b9f5ab1a16bf311b04442a4be7f04fda9e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-04 13:14:34 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@cfc9221

--- src/data-snowflake-current-account/index.ts
+++ src/data-snowflake-current-account/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.92.0/docs/data-sources/current_account
 // generated from terraform resource schema
 
