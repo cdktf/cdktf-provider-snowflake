8f30aebfafe6ee0975f0936d9bfca70275a2c02c
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-14 06:16:33 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/api-authentication-integration-with-jwt-bearer/index.ts
+++ src/api-authentication-integration-with-jwt-bearer/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.93.0/docs/resources/api_authentication_integration_with_jwt_bearer
 // generated from terraform resource schema
 
