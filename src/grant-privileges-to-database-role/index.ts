7477cbf95d4451b037b9ee69fd955498e0bd1709
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-14 22:13:20 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@fdf7d77

--- src/grant-privileges-to-database-role/index.ts
+++ src/grant-privileges-to-database-role/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.93.0/docs/resources/grant_privileges_to_database_role
 // generated from terraform resource schema
 
