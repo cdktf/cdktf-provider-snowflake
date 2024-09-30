bd47705219db567a352b49277312fdd8b849212f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-30 10:26:09 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/password-policy/index.ts
+++ src/password-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.96.0/docs/resources/password_policy
 // generated from terraform resource schema
 
