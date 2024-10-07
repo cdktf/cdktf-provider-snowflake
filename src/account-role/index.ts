96357793434b89d21a8d3e01ee49553db40d8bcc
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-07 08:26:29 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@ec8a1f2

--- src/account-role/index.ts
+++ src/account-role/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.96.0/docs/resources/account_role
 // generated from terraform resource schema
 
