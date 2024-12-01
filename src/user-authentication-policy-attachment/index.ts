0f61575ad5c1e697ca43cdbb87447881ca1f936e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-01 06:14:48 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@bf4e5ae

--- src/user-authentication-policy-attachment/index.ts
+++ src/user-authentication-policy-attachment/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/0.99.0/docs/resources/user_authentication_policy_attachment
 // generated from terraform resource schema
 
