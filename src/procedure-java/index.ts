2bd5deb1f18cb967d38f19746a00328cbab57572
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-06 15:45:47 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@cfd6f60

--- src/procedure-java/index.ts
+++ src/procedure-java/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/1.0.1/docs/resources/procedure_java
 // generated from terraform resource schema
 
