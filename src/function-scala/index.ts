945e6c21d19f443f34865522224b24c2e41c1a2f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-15 12:38:49 +0000

    chore(deps): update  JSII version to

--- src/function-scala/index.ts
+++ src/function-scala/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflake-labs/snowflake/1.0.1/docs/resources/function_scala
 // generated from terraform resource schema
 
