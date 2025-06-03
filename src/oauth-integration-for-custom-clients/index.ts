4727d292f72cd7a72ae79b1f88eb644202143c1f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:58:29 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/oauth-integration-for-custom-clients/index.ts
+++ src/oauth-integration-for-custom-clients/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflakedb/snowflake/2.1.0/docs/resources/oauth_integration_for_custom_clients
 // generated from terraform resource schema
 
