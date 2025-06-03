45aa020b3817872922732027841c2e9d0dda9122
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:55:40 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/data-snowflake-cortex-search-services/index.ts
+++ src/data-snowflake-cortex-search-services/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/snowflakedb/snowflake/2.1.0/docs/data-sources/cortex_search_services
 // generated from terraform resource schema
 
