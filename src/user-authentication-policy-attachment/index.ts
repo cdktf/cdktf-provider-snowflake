51e6f22d6edc57a3595ca935bf1df5023cf1f71c
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:50:58 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/user-authentication-policy-attachment/index.ts
+++ src/user-authentication-policy-attachment/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/snowflakedb/snowflake/2.1.0/docs/resources/user_authentication_policy_attachment
 // generated from terraform resource schema
 
